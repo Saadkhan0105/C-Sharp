1e95d28b4c38321f2854548f47b6feade1c0a97e
Saad Khan <saadkhan9560@gmail.com>
2025-08-28 18:04:51 +0530

    String Iteration (Looping)

---
+++ String Iteration (looping)/String Iteration (looping)/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace String_Iteration__looping_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "C# is awesome";
+
+            //Console.WriteLine(message[0]);
+            //Console.WriteLine(message[1]);
+            //Console.WriteLine(message[2]);
+            //Console.WriteLine(message[3]);
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(message.Contains("C"));
+
+            bool contains = false;
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (message[i].Equals('C'))
+                {
+                    contains = true;
+                    break;
+                }
+
+            }
+
+            Console.WriteLine(contains);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
