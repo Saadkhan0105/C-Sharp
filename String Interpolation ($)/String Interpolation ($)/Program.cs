50a860815e51ad6efd3ba79e846c626ab26a0445
Saad Khan <saadkhan9560@gmail.com>
2025-08-28 16:56:15 +0530

    String Interpolation ($)

---
+++ String Interpolation ($)/String Interpolation ($)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Interpolation____
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Saad";
+            int age = 29;
+
+            Console.WriteLine("Your name is " + name + " your age is " + age);
+            Console.WriteLine($"Your name is {name} and your age is {age}");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
