b67b40e02686fbdda8d9f4928f10a586c1e38658
Saad Khan <saadkhan9560@gmail.com>
2025-08-28 21:26:49 +0530

    Clearing an Array

--- Array Clearing/Array Clearing/Program.cs
+++ Array Clearing/Array Clearing/Program.cs
@@ -14,13 +14,12 @@ namespace Array_Clearing
             int[] numbers = new int[]
             {
                 0,1,2,3,4,5,6,7,8,9
-
             };
 
             //Array.Clear(numbers, 0, numbers.Length);
             Array.Clear(numbers, 5, 5);
 
-            for (int i = 0; i < numbers.Length; i++)
+            for (int i = 0; i < 10; i++)
             {
                 numbers[i] = default;
             }
