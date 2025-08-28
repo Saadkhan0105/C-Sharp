3acdd6de9fc9396091b203e4fa4ad14b3ffc5149
Saad Khan <saadkhan9560@gmail.com>
2025-08-28 21:25:22 +0530

    Clearing an Array

---
+++ Array Clearing/Array Clearing/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Clearing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[]
+            {
+                0,1,2,3,4,5,6,7,8,9
+
+            };
+
+            //Array.Clear(numbers, 0, numbers.Length);
+            Array.Clear(numbers, 5, 5);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = default;
+            }
+            foreach (var item in numbers)
+            {
+                Console.Write($"{item} ");
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
