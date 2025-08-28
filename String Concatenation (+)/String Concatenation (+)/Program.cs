d704a7cb4accc6b7b5bc61b0699cc5b1614c5235
Saad Khan <saadkhan9560@gmail.com>
2025-08-28 17:03:41 +0530

    String Concatenation

---
+++ String Concatenation (+)/String Concatenation (+)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Concatenation____
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Saad";
+            int age = 29;
+
+            Console.WriteLine("Your name is " + name + " your age is " + age);
+            Console.WriteLine($"Your name is {name} and your age is {age}");
+            Console.WriteLine("Your name is {0} and your age is {1}", name, age);
+
+
+            Console.ReadLine();
+        }
+    }
+}
