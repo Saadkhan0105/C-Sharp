09d08b29fbce55efedb696e1755587845aeffc0d
Saad Khan <saadkhan9560@gmail.com>
2025-08-28 18:31:49 +0530

    String IsNullOrEmpty

---
+++ String IsNullOrEmpty Function/String IsNullOrEmpty Function/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_IsNullOrEmpty_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {name}");
+
+            //if (name != "")
+            //    Console.WriteLine("0");
+
+            //if (!name.Equals(""))
+            //    Console.WriteLine("1");
+
+            //if(!string.IsNullOrEmpty(name))
+            //    Console.WriteLine("2");
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                if (name.Equals("Saad"))
+                {
+                    Console.WriteLine("Correct");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
