1ba1c9693c69781dec15c262c0f6b675cf8ef559
Saad Khan <saadkhan9560@gmail.com>
2025-08-28 15:31:20 +0530

    Exercise - Print a Table using for loop

---
+++ Exercise - Times Table/Exercise - Times Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise___Times_Table
+{
+    internal class Program
+        /*
+         * Ask the user for a number for the table.
+         * Write a for loop to print X times table
+         */ 
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", i, number, i * number);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
