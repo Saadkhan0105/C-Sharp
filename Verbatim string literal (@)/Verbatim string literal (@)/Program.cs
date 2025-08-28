d9f8b19ece052098040839a6ea481e13b9207012
Saad Khan <saadkhan9560@gmail.com>
2025-08-28 16:29:15 +0530

    Verbatim Identifier

---
+++ Verbatim string literal (@)/Verbatim string literal (@)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verbatim_string_literal____
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // \t - tab \n - new line \\ - backslash
+
+            string speech = "Hello \nMy name is John \tI am a developer";
+            Console.WriteLine(speech);
+
+            string path = "C:\\Users\\Admin\\Desktop\\C - sharp";
+            Console.WriteLine(path);
+
+            path = @"C:\Users\Admin\Desktop\C - sharp";
+            Console.WriteLine(path);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Verbatim string literal (@)/Verbatim string literal (@)/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Verbatim string literal (@)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Verbatim string literal (@)")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ed9bffb-5cc0-4bff-aa9e-9c4dc5559a96")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
