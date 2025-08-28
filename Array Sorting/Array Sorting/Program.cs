dba6aa362909e39007c51885723208e78b573cb5
Saad Khan <saadkhan9560@gmail.com>
2025-08-28 20:41:50 +0530

    Sorting in Arrays

---
+++ Array Sorting/Array Sorting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[]
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10
+            };
+
+            string test = "";
+            test.Replace(" ", "");
+
+            Array.Sort(numbers);
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
