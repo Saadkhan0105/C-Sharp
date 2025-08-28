f1b45be20a6d60361e7948342f7b84bbc5caac38
Saad Khan <saadkhan9560@gmail.com>
2025-08-28 20:22:13 +0530

    Exercise - Checking a Triangle

---
+++ Exercise - Checking a Triangle/Exercise - Checking a Triangle/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise___Checking_a_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int angleCount = 3;
+            int angleSum = 0;
+
+            for (int i = 0; i < angleCount; i++)
+            {
+                Console.Write($"Enter angle {i + 1}: ");
+                angleSum += Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine(angleSum == 180 ? "Valid" : "Invalid");
+
+            /*
+            Console.Write("Enter angle 1: ");
+            int angle1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter angle 2: ");
+            int angle2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter angle 3: ");
+            int angle3 = Convert.ToInt32(Console.ReadLine());
+            int agleSum = angle1 + angle2 + angle3;
+            if (agleSum == 180)
+            {
+                Console.WriteLine("The angles form a triangle.");
+            }
+            else
+            {
+                Console.WriteLine("The angles do not form a triangle.");
+            }
+            */
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercise - Checking a Triangle/Exercise - Checking a Triangle/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Exercise - Checking a Triangle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exercise - Checking a Triangle")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("72f86623-8816-46c9-8d84-cac30b8d6a2d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
