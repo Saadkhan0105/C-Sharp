c38cf2645ece5f2210ebff53d0118fe4a8768549
Saad Khan <saadkhan9560@gmail.com>
2025-08-28 16:43:14 +0530

    String Formatting

---
+++ String Formatting/String Formatting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Formatting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Saad";
+            int age = 29;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Name: " + name + "\nAge: " + age);
+
+            Console.ReadLine();
+        }
+    }
+}
