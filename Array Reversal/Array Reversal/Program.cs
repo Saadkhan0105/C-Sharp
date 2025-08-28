a53c44103690a3af4346dadea2298daba636aa05
Saad Khan <saadkhan9560@gmail.com>
2025-08-28 20:51:10 +0530

    Reversing an Array

---
+++ Array Reversal/Array Reversal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Reversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[]
+            { 
+                0, 1, 2, 3, 4, 5 
+            };
+
+            Array.Reverse(num);
+
+            foreach (var item in num)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
