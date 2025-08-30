a8d50dead2d398a5e792cb800618963d1d6f541d
Saad Khan <saadkhan9560@gmail.com>
2025-08-30 20:01:20 +0530

    Dictionary

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Dictionary<int, string> names = new Dictionary<int, string>
+            {
+                // Key value pairs
+                { 1, "Saad" },
+                { 2, "Test" },
+                { 3, "Test" }
+            };
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                KeyValuePair<int, string> pair = names.ElementAt(i);
+                Console.WriteLine($"{pair.Key} - {pair.Value}");
+
+            }
+
+            Console.WriteLine()
+                ;
+            foreach (KeyValuePair<int, string> item in names)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }*/
+
+            Dictionary<string, string> teachers = new Dictionary<string, string>
+            {
+                { "Math", "Saad" },
+                { "Science", "Test" }
+            };
+
+            Console.WriteLine(teachers["Math"]);
+
+            Console.ReadLine(); 
+        }
+    }
+}
