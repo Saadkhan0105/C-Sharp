3dd4735d9c672265e3d31f6cad6d73441adf4962
Saad Khan <saadkhan9560@gmail.com>
2025-08-30 18:15:12 +0530

    Lists

---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[3]
+            {
+                1, 2, 3 
+            };
+
+            List<int> listNumbers = new List<int>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Enter a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                listNumbers.Add(num);
+            }
+
+            for (int i = 0; i < listNumbers.Count; i++)
+            {
+                Console.WriteLine(listNumbers[i]);
+            }
+            listNumbers.RemoveAt(0);
+
+            foreach (var item in listNumbers)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
