c4abb222fd9dc7db2eba504d6867bca7f54ddabc
Saad Khan <saadkhan9560@gmail.com>
2025-09-01 17:43:51 +0530

    Optional Parameters

---
+++ Optional Parameters/Optional Parameters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Optional_Parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Add(5); // Both parameters provided
+            Console.WriteLine(result);
+
+            PrintName();
+
+            Console.ReadLine();
+        }
+        static void PrintName(string name = "Saad")
+        {
+            Console.WriteLine($"My name is {name}");
+        }
+        static int Add(int a, int b = default)
+        {
+            return a + b;
+        }
+    }
+}
