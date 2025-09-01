20171e5e7fa75572d19e9db178e99ec0a7eaa7b2
Saad Khan <saadkhan9560@gmail.com>
2025-09-01 14:52:59 +0530

    Void Function

---
+++ Void Function/Void Function/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WelcomeMessage();
+            CreateAndPrintArray();
+            Console.ReadLine();
+        }
+
+        static void CreateAndPrintArray()
+        {
+            int[] numbers = new int[5] { 0, 1, 2, 3, 4 };
+
+            foreach (var item in numbers)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to my program!");
+        }
+    }
+}
