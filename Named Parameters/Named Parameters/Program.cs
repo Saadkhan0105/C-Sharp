85386bff84d05de1fed16aca26e1e9cd60d1dc76
Saad Khan <saadkhan9560@gmail.com>
2025-09-01 18:10:49 +0530

    Named Parameters

---
+++ Named Parameters/Named Parameters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Named_Parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nameInput = "John";
+            int ageInput = 30;
+            string addressInput = "123 Main St";
+
+            PrintDetails(age: ageInput,
+                         name: nameInput , 
+                         address: addressInput );
+
+            Console.ReadLine();
+        }
+
+        static void PrintDetails(string name, int age, string address)
+        {
+            Console.WriteLine($"Name: {name} ");
+            Console.WriteLine($"Age: {age} ");
+            Console.WriteLine($"Address: {address}");
+        }
+    }
+}
