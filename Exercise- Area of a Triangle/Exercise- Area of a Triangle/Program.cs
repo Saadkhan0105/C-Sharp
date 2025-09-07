e83321815259124a13dcfab0657a10a7d90b09b7
Saad Khan <saadkhan9560@gmail.com>
2025-09-07 19:35:58 +0530

    Exercise- Area of a Triangle

---
+++ Exercise- Area of a Triangle/Exercise- Area of a Triangle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise__Area_of_a_Triangle
+{
+    /*
+     * Ask user for width and height and store them.
+     * Create function to calculate area.
+     * FUnction should calculate the area using: (width * height) / 2
+     * Call in main and print out the area of the tirangle.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int width = ReadInt("Enter the width of the triangle: ");
+            int height = ReadInt("Enter the height of the triangle: ");
+
+            Console.WriteLine($"The area of the triangle is: {CalculateArea(width, height)}");
+
+            Console.ReadLine();
+        }
+        static int CalculateArea(int width, int height)
+        {
+            return (width * height) / 2;
+        }
+        static int ReadInt(string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
