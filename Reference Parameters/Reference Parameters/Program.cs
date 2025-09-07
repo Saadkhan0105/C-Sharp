0a136e684c34d75f7ce4b8f428f5221f1ba61ac0
Saad Khan <saadkhan9560@gmail.com>
2025-09-07 18:34:49 +0530

    Reference Parameters

--- Reference Parameters/Reference Parameters/Program.cs
+++ Reference Parameters/Reference Parameters/Program.cs
@@ -10,14 +10,43 @@ namespace Reference_Parameters
     {
         static void Main(string[] args)
         {
+            /*
             int num = 10;
-            Assign(ref num);
+            string name = "Hello";
+            Assign(ref num, ref name);
             Console.WriteLine(num);
+            Console.WriteLine(name);
+            */
+
+            string name = "John";
+
+            Console.Write("Enter your new name: ");
+            string newName = Console.ReadLine();
+
+            if (ChangeName(ref name, newName))
+            {
+                Console.WriteLine($"Your new name is {name}!");
+            }
+            else
+            {
+                Console.WriteLine("New name cannot be empty or null!");
+            }
 
             Console.ReadLine();
         }
-        static void Assign(ref int  num)
+        
+        static bool ChangeName(ref string name, string newName)
         {
+            if (!string.IsNullOrEmpty(newName))
+            {
+                name = newName;
+                return true;
+            }
+                return false;
+        }
+        static void Assign(ref int  num, ref string name)
+        {
+            name = "Saad";
             num =  20;
         }
     }
