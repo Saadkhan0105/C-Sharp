3ed2c1710af98ea41beea43208dffb16b672fd16
Saad Khan <saadkhan9560@gmail.com>
2025-09-07 19:40:09 +0530

    Updating the file

--- AdoDemo/AdoDemo.ApiService/obj/Debug/net8.0/AdoDemo.ApiService.AssemblyInfo.cs
+++ AdoDemo/AdoDemo.ApiService/obj/Debug/net8.0/AdoDemo.ApiService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdoDemo.ApiService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e1138afdfb7084de9e2a95e44bd87a263308f9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a136e684c34d75f7ce4b8f428f5221f1ba61ac0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdoDemo.ApiService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdoDemo.ApiService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
