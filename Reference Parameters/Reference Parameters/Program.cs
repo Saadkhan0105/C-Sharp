77047aee9a5975881f34c23b11678d396f591b49
Saad Khan <saadkhan9560@gmail.com>
2025-09-07 18:15:22 +0530

    Reference Parameters

---
+++ Reference Parameters/Reference Parameters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reference_Parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            Assign(num);
+            Console.WriteLine(num);
+
+            Console.ReadLine();
+        }
+        static void Assign(int  num)
+        {
+            num =  20;
+        }
+    }
+}
